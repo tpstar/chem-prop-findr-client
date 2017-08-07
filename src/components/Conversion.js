d0ac7ad8e5e07aebf9dcb94121fdce16548cc9ed
tpstar <hanlee12@gmail.com>
2017-08-06 23:10:27 -0500

    make input box for conversion with dropdown units

--- src/components/Conversion.js
+++ src/components/Conversion.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react'
-import { DropdownButton, MenuItem } from 'react-bootstrap'
+import { DropdownButton, MenuItem, FormGroup, InputGroup, FormControl } from 'react-bootstrap'
 
 class Conversion extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      grams: 0
+      grams: 0,
+      inputUnit: "unit"
     }
   }
+
+  handleOnClick = inputUnit => {
+    this.setState({inputUnit})
+    console.log(inputUnit);
+  }
+
+  // onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
 
   render() {
 
@@ -20,14 +28,28 @@ class Conversion extends Component {
       <div className="row">
         <h3> Conversion </h3>
         <div className="conversion col-md-8 offset-md-4 col-sm-8 offset-sm-4">
-          <DropdownButton title="Dropdown">
-            <MenuItem href="#books">Books</MenuItem>
-            <MenuItem href="#podcasts">Podcasts</MenuItem>
-            <MenuItem href="#">Tech I Like</MenuItem>
-            <MenuItem href="#">About me</MenuItem>
-            <MenuItem href="#addBlog">Add a Blog</MenuItem>
-          </DropdownButton>
-        </div> {/* col-md-8 */}
+          <FormGroup>
+            <InputGroup>
+              <FormControl type="text" />
+              <DropdownButton
+                componentClass={InputGroup.Button}
+                id="input-dropdown-addon"
+                title={this.state.inputUnit}
+              >
+                <MenuItem
+                  onClick={() => this.handleOnClick("g")}
+                  key="grams"
+                  >g
+                </MenuItem>
+                <MenuItem
+                  onClick={() => this.handleOnClick("mol")}
+                  key="mol"
+                  >mol
+                </MenuItem>
+              </DropdownButton>
+            </InputGroup>
+          </FormGroup>
+        </div>
       </div>
     )
   }
